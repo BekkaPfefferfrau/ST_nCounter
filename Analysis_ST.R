7a62a28b78ad72e4f2c8754dc8edfd36d0802bee
BekkaPfefferfrau <rebekka.salzmann1@gmail.com>
2026-02-18 12:52:19 +0100

    Added background correction

--- Analysis_ST.R
+++ Analysis_ST.R
@@ -103,6 +103,10 @@ matrix3 <- (function(x) {
 #Multiplying the positive_ctrl factor from the function pos_ctrl_function with the respective values from each column with the respective pos_ctrl_factor with sampleID matching
 #removing all samples that failed the pos_ctrl_qc with sampleID matching
 
+##Scale to are
+#here we should do the first normalization step according to the ROI are but I do not have this information
+#--> need to ask collaborator to provide this
+
 
 ##Housekeeping QC
 
@@ -126,37 +130,69 @@ write.csv(
 #Rownames are taken from matrix3
 
 
-## Housekeeping data normalization (not necessary for our samples as they all passed but I will still write the code for the sake of completeness)
+## Housekeeping data normalization
+
+matrix4 <- (function(x) {
+  HK_genes <- matrix3[c("OAZ1","POLR2A","RAB7A","SDHA","UBB"), , drop = FALSE]
+  HK_ctrl <- colSums(HK_genes)
+  HK_ctrl_median <- median(HK_ctrl)
+  HK_ctrl_factor <- HK_ctrl_median / HK_ctrl
+  HK_ctrl_qc <- ifelse(HK_ctrl_factor >= 0.1 & HK_ctrl_factor <= 10, "PASS", "FAIL")
+  cols <- intersect(colnames(x), names(HK_ctrl_factor))
+  sweep(
+    x[!rownames(x) %in% c("OAZ1","POLR2A","RAB7A","SDHA","UBB"), cols, drop = FALSE],
+    2,
+    HK_ctrl_factor[cols],
+    `*`
+  )[, HK_ctrl_qc[cols] == "PASS", drop = FALSE]
+})(matrix3)
 
 
+## Negative control data normalization
+
+matrix5 <- (function(x) {
+  neg_ctrl <- matrix4[c("NegPrb1", "NegPrb2", "NegPrb3", "NegPrb4", "NegPrb5"), ]
+  neg_ctrl <- colSums(neg_ctrl)
+  neg_ctrl_median <- median(neg_ctrl)
+  neg_ctrl_factor <- neg_ctrl_median / neg_ctrl
+  cols <- intersect(colnames(x), names(neg_ctrl_factor))
+  sweep(
+    x[!rownames(x) %in% c("NegPrb1", "NegPrb2", "NegPrb3", "NegPrb4", "NegPrb5"), cols, drop = FALSE],
+    2,
+    neg_ctrl_factor[cols],
+    '*')
+})(matrix4)
 
 
+## Negative hybridization control
 
-
-
-
-## Negative control QC
-neg_control = matrix2[c("NegPrb1", "NegPrb2", "NegPrb3", "NegPrb4", "NegPrb5"), ]
 
 # Annotation
-annotation1 = read.table("C:/ST/annotation.txt", sep = "\t", header = TRUE, colClasses="character")
-rn2 = annotation1[,c(1:3)]
-rn2 = apply(rn2,1,function(x) paste(x[1:3], collapse = ("_")))
-annotation1 = annotation1[,-c(1:3,5:8)]
-rownames(annotation1) = rn2
-common_rois <- intersect(colnames(matrix3), rownames(annotation1))
-annotation2 <- annotation1[common_rois, ]
+annotation1 <- read.table("C:/ST/annotation.txt", sep = "\t", header = TRUE, colClasses="character")
+annotation2 <- (function(x){
+  rn <- apply(x[, 1:3], 1, paste, collapse = "_")
+  rownames(x) <- rn
+  x <- x[, -c(1:3, 5:8), drop = FALSE]
+  cols <- intersect(rownames(x), colnames(matrix5))
+  x <- x[cols, , drop = FALSE]
+  return(x)
+})(annotation1)
+
 
 #Visualizing first part just for fun
-sorted_cols <- rownames(annotation2)[order(annotation2$distance)]
-matrix3 <- matrix3[, sorted_cols]
-pheatmap(matrix3,
-         scale = "none", color = colorRampPalette(c("blue","white","green"))(100),
-         breaks = seq(min(matrix3),
-                      max(matrix3), length.out = 101),
+matrix_sort <- (function(x){
+  sorted_cols <- rownames(annotation2)[order(annotation2$distance)]
+  matrix <- matrix5[, sorted_cols]
+  pheatmap(matrix,
+         scale = "none",
+         color = colorRampPalette(c("white","blue"))(100),
+         breaks = seq(min(matrix5),
+                      max(matrix5),
+                      length.out = 100),
          cluster_cols = FALSE,
          cluster_rows = TRUE,
          fontsize_row = 6,
          fontsize_col = 6,
          width = 15,
          height = 10)
+  })(matrix5)
