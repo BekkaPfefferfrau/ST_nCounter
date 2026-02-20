0716024c08f2e081764f23822989749af67f8bf3
BekkaPfefferfrau <rebekka.salzmann1@gmail.com>
2026-02-20 15:03:44 +0100

    bla

--- Spatial_transcriptomics_GeoMX_DSP_nCounter_readout/ui.R
+++ Spatial_transcriptomics_GeoMX_DSP_nCounter_readout/ui.R
@@ -9,25 +9,19 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-fluidPage(
+ui <- pageWithSidebar(
+  
+  headerPanel("GeoMX DSP nCounter"),
+  
+   sidebarPanel(
+     selectInput("variable", "Variable:",
+                c("Housekeeping" = "HK",
+                  "Background" = "backgorund",
+                  "#Nuclei" = "SCALEnuclei",
+                  "AOI" = "SCALEarea")),
+     checkboxInput("QC", "Show outliers", TRUE)
+   
+     ),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+  mainPanel()
 )
